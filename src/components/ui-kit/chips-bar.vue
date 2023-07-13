756ab7f451e81340cb67a0ac1f51eb3b054775a0
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-07-13 09:15:18 +0500

    fix display icon on production

--- src/components/ui-kit/chips-bar.vue
+++ src/components/ui-kit/chips-bar.vue
@@ -28,7 +28,7 @@
         <img
           class="chip-icon"
           @click="() => (desktop ? handleEdit(true) : activeEdit(true))"
-          :src="`/src/assets/images/iconseditoutline-${
+          :src="`src/assets/images/iconseditoutline-${
             activeChip === i ? 'white.svg' : 'black.svg'
           }`"
         />
