cb1d3105a405b7a19fe3f9ff677ab38afbce5a49
Alexander Draginsky <alexander.draginsky@gmail.com>
2023-11-03 22:01:27 +0300

    refactor: formatting; delete unused files

--- src/stores/paperData.ts
+++ src/stores/paperData.ts
@@ -4,27 +4,27 @@ import { type PaperData } from "@/types/paper.types";
 
 export const usePaperDataStore = defineStore("paperData", () => {
   const paperDTO = ref<PaperData>({
-    "country": "",
-    "registration_form": "",
-    "name": "",
-    "registration_number": 0,
-    "address": {
-      "region": "",
-      "neighborhood": "",
-      "city": "",
-      "street": "",
-      "house": "",
-      "housing": "",
-      "building": "",
-      "apartment": "",
-      "index": 0
+    country: "",
+    registration_form: "",
+    name: "",
+    registration_number: 0,
+    address: {
+      region: "",
+      neighborhood: "",
+      city: "",
+      street: "",
+      house: "",
+      housing: "",
+      building: "",
+      apartment: "",
+      index: 0,
     },
-    "website": "",
-    "first_photo": "",
-    "second_photo": "",
-    "third_photo": "",
-    "work_position": "",
-    "ownership": true
+    website: "",
+    first_photo: "",
+    second_photo: "",
+    third_photo: "",
+    work_position: "",
+    ownership: false,
   });
 
   return {
