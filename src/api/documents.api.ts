16e4d56d5a7a3d6623c1607227f2263bfc47d16b
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-11-04 00:09:02 +0500

    update payload for docs api

--- src/api/documents.api.ts
+++ src/api/documents.api.ts
@@ -1,11 +1,13 @@
 import { baseApiClient } from "./baseApiClient";
 import { Passport, Paper } from "@/types/documents.types";
+import { PassportData } from "@/types/passport.type";
+import { PaperData } from "@/types/paper.types";
 
 const DOC_END_POINT = "/documents";
 
-export const passports = (payload: Passport) => {
+export const passports = (payload: Passport | PassportData) => {
     return baseApiClient.post(DOC_END_POINT + "/passports/", payload);
 }
-export const papers = (payload: Paper) => {
+export const papers = (payload: Paper | PaperData) => {
     return baseApiClient.post(DOC_END_POINT + "/papers/", payload);
 }
