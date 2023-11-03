9dfa4095bf0988026394ac7b3707807b38ef88f9
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-11-03 18:12:27 +0500

    add address type

---
+++ src/types/address.type.ts
@@ -0,0 +1,11 @@
+export type  Address = {
+    "region": string,
+    "neighborhood": string,
+    "city": string,
+    "street": string,
+    "house": string,
+    "housing": string,
+    "building": string,
+    "apartment": string,
+    "index": 0
+}
