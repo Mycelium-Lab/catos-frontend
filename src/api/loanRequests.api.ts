65501af4d54f8732375791ec1f3fe22104253bcb
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-11-10 11:13:02 +0500

    chore: loans

---
+++ src/api/loanRequests.api.ts
@@ -0,0 +1,20 @@
+import { baseApiClient } from "./baseApiClient";
+import { LoansRequestPayload, LoansRequestResponse } from "../types/loan.types"
+
+const LOAN_REQUESTS_END_POINT = "/loanRequests";
+
+export const listLoanRequest = () => {
+    return baseApiClient.get<LoansRequestResponse[]>(LOAN_REQUESTS_END_POINT)
+}
+
+export const createLoanRequest = (payload: LoansRequestPayload) => {
+    return baseApiClient.post(LOAN_REQUESTS_END_POINT, payload);
+};
+
+export const retrieveLoanRequest = (id: number) => {
+    return baseApiClient.get(`${LOAN_REQUESTS_END_POINT}/${id}`)
+}
+
+export const approveLoanRequest = (id: number) => {
+    return baseApiClient.patch(`${LOAN_REQUESTS_END_POINT}/${id}`)
+}
