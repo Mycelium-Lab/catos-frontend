a7abe6f1b99a1f70d7056d64ebd778ca80d94a5b
Alexander Draginsky <alexander.draginsky@gmail.com>
2023-11-14 00:45:16 +0300

    chore: pool invest api

--- src/api/pools.api.ts
+++ src/api/pools.api.ts
@@ -3,19 +3,23 @@ import { Pool } from "@/types/pool.type";
 import { CreatePoolData } from "@/types/pool.type";
 const POOL_END_POINT = "/pool";
 
+export type LiquidityPayload = {
+  pool_id: number;
+  amount: number;
+}; 
+
 export const listPools = () => {
   return baseApiClient.get<Pool[]>(POOL_END_POINT);
 };
-
 export const createPool = (payload: CreatePoolData) => {
   return baseApiClient.post(POOL_END_POINT, payload);
 };
-export const investToPool = () => {
-  return baseApiClient.post(POOL_END_POINT + '/invest');
+export const investToPool = (payload: LiquidityPayload) => {
+  return baseApiClient.post(POOL_END_POINT + '/invest', payload);
 };
-export const depositToPool = (payload: CreatePoolData) => {
+export const depositToPool = (payload: LiquidityPayload) => {
   return baseApiClient.post(POOL_END_POINT + '/deposit', payload);
 };
-export const withdrawFromPool = (payload: CreatePoolData) => {
+export const withdrawFromPool = (payload: LiquidityPayload) => {
   return baseApiClient.post(POOL_END_POINT + '/withdraw', payload);
 };
