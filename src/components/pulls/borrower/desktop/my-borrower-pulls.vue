effc0474eb2aaefff98c4682655962cf56459ba2
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2023-11-14 06:34:02 +0500

    upd: component of own pool for borrower

--- src/components/pulls/borrower/desktop/my-borrower-pulls.vue
+++ src/components/pulls/borrower/desktop/my-borrower-pulls.vue
@@ -21,51 +21,43 @@
     <add v-if="isAdd" @close="() => {
         close()
     }"
-    @qr="handleTransaction"
+    :poolId="poolId"
     >
 
     </add>
 
     <withdraw v-if="isWidthdraw" @close="() => {
          close()
-    }"
-    @qr="handleTransaction"
-    >
+    }">
     </withdraw>
     
-    <transaction-desktop v-if="isTransaction" 
+    <!--<transaction-desktop v-if="isTransaction" 
     @close="close"
     :title="isAdd ? 'Подтвердите добавление ликвидности' : 'Подтвердите изъятие ликвидности'"
     :subtitle="isAdd ? 'Пожалуйста, подтвердите доавление ликвидности в своем кошельке' 
     : 'Пожалуйста, подтвердите изъятие ликвидности в своем кошельке'"
     >
-    </transaction-desktop>
+    </transaction-desktop>-->
   
   </template>
   
   <script setup lang="ts">
   import { ref } from "vue";
   import detail from "./modal-body/detail.vue";
-  import getLoan from "./modal-body/get-loan.vue";
-  import invest from "./modal-body/invest.vue";
-  import desktopModal from "@/components/base/desktop-modal.vue";
-  import getLoanSuccess from "@/components/pulls/borrower/desktop/modal-body/get-loan-success.vue";
-  import statusModalDesktop from "@/components/base/status-modal-desktop.vue";
-  import loader from "@/components/base/loader.vue"
   import add from "@/components/pulls/creditor/desktop/modal-body/add.vue"
   import withdraw from "@/components/pulls/creditor/desktop/modal-body/withdraw.vue"
-  import transactionDesktop from "@/components/base/modals/transaction-desktop.vue";
   
-  const { state } = defineProps({
+  const { state, poolId } = defineProps({
     state: {
       type: Object,
       required: true,
     },
+    poolId: {type: Number, required: true}
   });
+
   const isDetailMy = ref(state.detailMyModal);
   const isAdd = ref(false)
   const isWidthdraw = ref(false)
-  const isTransaction = ref(false)
   
   const emits = defineEmits(["close", "get-loan"]);
   
@@ -73,12 +65,7 @@
     emits("close");
     isAdd.value = false
     isWidthdraw.value = false
-    isTransaction.value = false
   };
-
-  const handleTransaction = () => {
-    isTransaction.value = true;
-  }
   
   </script>
   
